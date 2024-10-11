5bed4f9ee4b7c9d879e2c71648fde61255d8a94c
MarceloOrtegaDev <ortegamarcelodavid77@gmail.com>
2024-10-11 14:09:43 -0300

    Actualizado

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,12 +3,10 @@ export const usersRouter = Router()
 import { getUserById, Logout, loginUser, registerUser, getSession } from "../controllers/auth.controllers.js"; 
 import { validarJwt } from "../middleware/validarJwt.js";
 
-usersRouter.get('/:id', getUserById );
-
-usersRouter.get("/", validarJwt, getSession);
 
 usersRouter.post('/register', registerUser );
-
 usersRouter.post('/login', loginUser );
-
 usersRouter.post('/logout',Logout );
+usersRouter.get("/session", validarJwt, getSession);
+usersRouter.get('/:id', getUserById );
+
